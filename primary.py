e3b566d91fb5e6983004d835ff7bce39b68fa1b7
evo <thetrueevo@gmail.com>
2020-10-24 16:45:46 -0400

    Initial demo code for sending a very basic request and getting a response

--- primary.py
+++ primary.py
@@ -1,30 +1,24 @@
+import socket
+import socketutil
+import time
+
+
 class HTTPRequest:
     def __init__(self):
-        self.method = ""  # GET, POST, PUT, etc. for this request
-        self.path = ""    # url path for this request
-        self.version = "" # http version for this request
-        self.headers = [] # headers from client for this request
-        self.length = 0   # length of the request body, if any
-        self.body = None  # contents of the request body, if any
+        self.method = ""
+        self.path = ""
+        self.version = ""
+        self.headers = []
+        self.length = 0
+        self.body = None
 
 
 class HTTPResponse:
     def __init__(self, code, mime_type=None, body=None):
         self.code = code
         self.mime_type = mime_type
+        self.headers = []
         self.body = body
-
-
-def get_rtt_to_host(url):
-    return
-
-
-def get_html_of_host(url):
-    return
-
-
-def get_html_file(path):
-    return
 
 
 def parse_url_parts(url):
@@ -33,9 +27,59 @@ def parse_url_parts(url):
     parts.append(url_prot_rem[0])
     url_path_rem = url_prot_rem[1].split("/", 1)
     parts.append(url_path_rem[0])
-    parts.append("/"+url_path_rem[1])
+    if len(url_path_rem) < 2:
+        parts.append("/")
+    else:
+        parts.append("/"+url_path_rem[1])
     return parts
 
 
-url = "https://google.com/"
-print(parse_url_parts(url))
+def send_http_request(req, target, sendport):
+    addr = (target, sendport)
+    s = socketutil.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(addr)
+    data = req.method + " " + req.path + " " + req.version + "\r\n"
+    data += "Host: "+target
+    data += "\r\n\r\n"
+    print(data)
+
+    s.sendall(data)
+    starttime = time.monotonic()
+    print("Start Time: " + str(starttime))
+    print("Sent data, awaiting response...")
+    responsedata = s.recv_until("\r\n\r\n")
+    if responsedata is None:
+        return
+    endtime = time.monotonic()
+    print("End Time: " + str(endtime))
+    print("Response received!")
+
+    duration = endtime - starttime
+
+    print()
+    print(responsedata.decode())
+    duration = duration * 1000
+    print()
+    print("Raw Time Elapsed: "+str(duration))
+    print("Approx. Time Elapsed (rtt): "+str(round(duration, 2)) + "ms")
+
+
+target_url = "http://google.com/"
+url_parts = parse_url_parts(target_url)
+print(url_parts)
+print()
+
+demo = HTTPRequest()
+demo.method = "HEAD"
+demo.path = url_parts[2]
+demo.version = "HTTP/1.1"
+
+if url_parts[0] == "https":
+    port = 443
+else:
+    port = 80
+
+send_http_request(demo, url_parts[1], port)
+
+
+
