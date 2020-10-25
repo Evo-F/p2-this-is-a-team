87ff4ea946f7700fc290e94f9303213f14e4c4e7
evo <thetrueevo@gmail.com>
2020-10-25 14:20:03 -0400

    Fixes bad arg parsing

--- primary.py
+++ primary.py
@@ -223,7 +223,7 @@ def send_http_response(sock, resp, keepalive):
 
 def serve_html_file(path):
     if path.startswith("/analyze"):
-        analysis_args = path.split("?", 1)
+        analysis_args = path.split("?", 1)[1]
         analysis_args = urllib.parse.unquote(analysis_args)
         print(analysis_args)
 
