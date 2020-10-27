66d0abb18f3621f9e420f0414cf4a0bf14606564
evo <thetrueevo@gmail.com>
2020-10-27 18:08:16 -0400

    Attempt to fix erroneous 403 5

--- primary.py
+++ primary.py
@@ -495,7 +495,8 @@ def serve_index():
         print("Server Count: %d" % (len(known_contacts)+1))
         print("Server List: (see above)")
         print(datastring)
-        datastring = datastring.format(serverlist=all_nodes_listified)
+        datastring = datastring.format(currentserver=cloud.dnsname,
+                                       servercount=len(known_contacts)+1)
         print("Finished formatting!")
         data = datastring.encode()
         return HTTPResponse("200 OK", "text/html", data)
